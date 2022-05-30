c764581bc2286d7227a99a0fffe82024c162a58b
Muhammad Hamza <84866952+Hamza2Dam@users.noreply.github.com>
2022-05-30 22:21:59 +0200

    d

--- Assets/ScriptsMenu/ShopController.cs
+++ Assets/ScriptsMenu/ShopController.cs
@@ -6,20 +6,26 @@ using UnityEngine.UI;
 
 public class ShopController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject DiamondCanvas;
 
-    // Update is called once per frame
-    void Update()
+
+    private void Start()
     {
-        
+        DiamondCanvas.SetActive(false); // Desactivar el canvas de Diamond
     }
     public void Back()
     {
-        SceneManager.LoadScene("MenuPrincipal");
+        SceneManager.LoadScene("MenuPrincipal"); // Cargar Menu Principal
+    }
+
+    public void OpenDiamondWindow()
+    {
+        DiamondCanvas.SetActive(true); // Activar el canvas de Diamond
+    }
+
+    public void CloseDiamondWindow()
+    {
+        DiamondCanvas.SetActive(false); // Desactivar el canvas de Diamond
     }
 
 }
